0d38388dd1260c8156866d823a560de0699c6f80
gnuhc <jonnnc@gmail.com>
2022-10-31 21:23:01 -0700

    Add json file matcher logic

--- src/services/parsing/fileMatchers/JsonFileMatcher.ts
+++ src/services/parsing/fileMatchers/JsonFileMatcher.ts
@@ -7,9 +7,15 @@ import { BaseFileMatcher } from "./BaseFileMatcher";
 @injectable()
 export class JsonFileMatcher extends BaseFileMatcher {
 
+    protected isExtraConfig() {
+        const filename = this.editor.document.fileName.replace(this.workspaceRoot, '');
+        const isExtra = this.config.extraFiles.includes(filename);
+        return isExtra;
+    }
+
     match() {
         if (
-            this.isExtraConfig('json') ||
+            this.isExtraConfig() ||
             this.isMatch('json', '**/.eslintrc.json', '**/eslintrc.json')
         ) {
             this.log.debug(`Using JsonParser for ${this.editor.document.fileName}.`);
